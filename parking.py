f6274d04df2569b62e157b4e503dc1eed54bc0fb
MaximBitiu <93537212+MaximBitiu@users.noreply.github.com>
2021-11-30 20:31:21 +0300

    Add files via upload

---
+++ parking.py
@@ -0,0 +1,18 @@
+parking = [0,0,0,0,1,1,0]
+
+index_to_park = int(input("Where? "))
+if index_to_park in range(0,len(parking)):
+    if parking[index_to_park] == 1:
+        print("Occupied!!!")
+    else:
+        print("You can park there!")
+        parking[index_to_park] = 1
+else:
+    print("Wrong option!")
+
+
+for index in range(0,len(parking)):
+    if parking[index] == 1:
+        print(index,"[ x ]")
+    elif parking[index] == 0:
+        print(index,"[   ]")
